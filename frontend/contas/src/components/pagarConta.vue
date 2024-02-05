ee46714e7ba2ba38bc6cf7f80289b5ea0d1b63e2
Natan Oliveira <natanoliveirati@gmail.com>
2024-02-05 15:35:41 -0300

    front

---
+++ frontend/contas/src/components/pagarConta.vue
@@ -0,0 +1,30 @@
+<!-- components/ApiButton.vue -->
+<template>
+  <button @click="pagar">Disparar API</button>
+</template>
+  
+  <script>
+import axios from "axios";
+
+export default {
+  methods: {
+    async pagar() {
+      try {
+        const response = await axios.post(
+          "https://localhost:3000/api/conta-pagar/pagar",
+          {}
+        );
+        console.log("Resposta da API:", response.data);
+        // Adicione qualquer lógica adicional aqui
+      } catch (error) {
+        console.error("Erro ao chamar a API:", error);
+      }
+    },
+  },
+};
+</script>
+  
+  <style scoped>
+/* Estilos específicos para este componente */
+</style>
+  
